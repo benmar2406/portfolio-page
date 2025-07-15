fcb50b17593ffbce843859162ea14dfb6356a107
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-15 22:02:56 +0200

    upd

--- src/lib/components/projectDetails/posterGallery.svelte
+++ src/lib/components/projectDetails/posterGallery.svelte
@@ -3,5 +3,12 @@
 </script>
 
 <div class="carousel-container">
-    
+
+    {#each posters as poster}
+        <img 
+            class="poster" 
+            src={poster}
+        >
+    {/each}
+
 </div>
