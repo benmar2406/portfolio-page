7d51b5cd57f151727441bd300689be489ffc2f02
benmar2406 <113034313+benmar2406@users.noreply.github.com>
2025-07-16 21:55:56 +0200

    up

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,6 +2,7 @@
     import '../../app.css'
     import Header from '$lib/components/Header.svelte';
     import Footer from '$lib/components/Footer.svelte';
+    
     let { children } = $props();
 </script>
 <div class="layout-grid">
